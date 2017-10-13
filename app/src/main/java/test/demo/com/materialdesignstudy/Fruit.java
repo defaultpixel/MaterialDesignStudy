732652072117f285c8984ef0e4a831618421d1b9
CPU:i5-6400 <1305048012@qq.com>
2017-10-13 17:57:35 +0800

    material design study

---
+++ app/src/main/java/test/demo/com/materialdesignstudy/Fruit.java
@@ -0,0 +1,20 @@
+package test.demo.com.materialdesignstudy;
+
+/**
+ * Created by Administrator on 2017/10/13.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+    public Fruit(String name, int imageId){
+        this.name = name;
+        this.imageId = imageId;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getImageId(){
+        return imageId;
+    }
+}
